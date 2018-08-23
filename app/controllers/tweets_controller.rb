0cf8ec2c9a9a2d29ca3f90c06d213bb948b61019
Trac Nguyen <nguyendtrac@gmail.com>
2018-08-23 14:54:59 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -34,4 +34,13 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/:id' do
+  if logged_in?
+    @tweet = Tweet.find_by_id(params[:id])
+    erb :'tweets/show_tweet'
+  else
+    redirect to '/login'
+  end
+end
+
 end
