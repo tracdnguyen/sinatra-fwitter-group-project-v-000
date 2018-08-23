1416ddcb66250144e36a0baf9a27b6a8e0b1fc82
Trac Nguyen <nguyendtrac@gmail.com>
2018-08-23 14:58:11 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180823143922) do
+ActiveRecord::Schema.define(version: 20180823145740) do
 
   create_table "tweet", force: :cascade do |t|
     t.string  "content"
