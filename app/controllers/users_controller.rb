2a15fe4a2d0dc6c7720853f03fd200f9bf1c9a5f
Trac Nguyen <nguyendtrac@gmail.com>
2018-08-23 15:14:44 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   get '/signup' do
     if !logged_in?
-      erb :'tweets'
+      erb :'users/create_user'
     else
       redirect to '/tweets'
     end
