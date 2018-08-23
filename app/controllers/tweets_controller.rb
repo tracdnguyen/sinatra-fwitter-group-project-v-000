60052d3804c9e235a2cbe08fda9e30a28f92aaf3
Trac Nguyen <nguyendtrac@gmail.com>
2018-08-23 14:51:42 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -17,4 +17,21 @@ class TweetsController < ApplicationController
     end
   end
 
+  post '/tweets' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/tweets/new"
+      else
+        @tweet = current_user.tweets.build(content: params[:content])
+        if @tweet.save
+          redirect to "/tweets/#{@tweet.id}"
+        else
+          redirect to "/tweets/new"
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
 end
